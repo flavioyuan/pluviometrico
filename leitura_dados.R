3801dbd438558e3999793c333c361844a2b666a1
flavioyuan <fyuangouvea@gmail.com>
2026-03-23 17:02:30 -0300

    Rebase

--- leitura_dados.R
+++ leitura_dados.R
@@ -1,5 +1,9 @@
 require(ggplot2)
 require(tidyverse)
 
+# Teste de leitura dos dados
 setwd("/home/flavioyuan/Dados/DadosPluviometricos")
 my_data <- read_csv("anchieta_199701_Plv.txt")
+
+# Read all files from folder
+files <- list.files(pattern = "*.txt")
