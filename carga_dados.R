a56c451eb1ce069a5e085ef46fc19172b7a71059
flavioyuan <fyuangouvea@gmail.com>
2026-03-23 23:36:08 -0300

    Update: Nome do arquivo capturado

--- carga_dados.R
+++ carga_dados.R
@@ -1,10 +1,12 @@
 # leitura dos dados pluviometricos e gravacao em arquivo unico
 #
+require(stringr)
 data_folder <- "/home/flavioyuan/Documentos/R/Projetos/pluviometrico/Dados"
 
 #Read all files from data folder
 files <- list.files(data_folder, pattern = "*.txt")
 
 for (i in 1:length(files)) {
-  print(files[i])
+  print(substring(files[i], 1, str_length(files[i]) - 15))
+  dados_pluviometricos <- read.csv(paste0(data_folder, "/", files[i]))
 }
